a8cd56b1e13239c16dd65ca678d69f3a8d9717ac
Miguel Angel Ceballos <miguelceballos2112@gmail.com>
2025-10-02 15:01:40 -0500

    Cambio de nombre de un archivo

--- src/main/java/co/edu/uniquindio/escuelademusica/App.java
+++ src/main/java/co/edu/uniquindio/escuelademusica/App.java
@@ -2,7 +2,7 @@ package co.edu.uniquindio.escuelademusica;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class App {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
